8e2ade09edd32ba123bc2fe4c9cd64bb00e4dc70
awtnb <arikaebanataw@gmail.com>
2025-03-06 16:11:00 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # README
 
-go build and copy exe to directory specified by `.env` .
+Build go project and copy executable to directory specified with commandline argument.
+
+```
+.\build.ps1 "$env:USERPROFILE\Personal\tools\bin"
+```
+
+## Install
 
 ```PowerShell
 irm https://raw.githubusercontent.com/AWtnb/gobuilder/refs/heads/main/main.ps1 | iex
--- main.ps1
+++ main.ps1
@@ -1,13 +1,12 @@
 @'
-go build
+$d = $args[0]
+if ($d.length -lt 1) {
+    Write-Host "Specify directory to copy executable."
+}
+else {
+    go build
 
-if ($LASTEXITCODE -eq 0) {
-    if (Test-Path .\.env) {
-        $d = Get-Content .\.env -Raw
-        if (-not $d -or $d.Trim().Length -lt 1) {
-            $d = $env:USERPROFILE | Join-Path -ChildPath "Personal\tools\bin"
-        }
-        $d = [System.Environment]::ExpandEnvironmentVariables($d)
+    if ($LASTEXITCODE -eq 0) {
         if (-not (Test-Path $d -PathType Container)) {
             New-Item -Path $d -ItemType Directory
         }
@@ -22,25 +21,7 @@ if ($LASTEXITCODE -eq 0) {
         }
     }
     else {
-        ".env not found!" | Write-Host -ForegroundColor Magenta
+        "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
     }
-}
-else {
-    "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
 }
 '@ | Out-File -Path "build.ps1" -Force
-New-Item -Path ".env" -ItemType File -ErrorAction SilentlyContinue
-
-$gi = ".gitignore"
-$lines = @("*.env", "*.exe")
-if (Test-Path $gi) {
-    $lines | ForEach-Object {
-        $p = $_
-        if (-not (Select-String -Path $gi -Pattern $p -SimpleMatch)) {
-            Write-Output $p | Out-File -FilePath $gi -Append
-        }
-    }
-}
-else {
-    $lines | Out-File -FilePath $gi
-}
